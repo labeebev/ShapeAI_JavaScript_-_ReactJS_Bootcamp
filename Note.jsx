eced2d5a54433c62849fe37775a7fb69f2a051cb
Labeeb EV <labeebev10@gmail.com>
2021-06-06 11:38:11 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        I am Labeeb EV,and an undergraduate student.This was an amazing
+        bootcamp. We covered basics of JavaScript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
